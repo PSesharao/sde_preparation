33c683cf876b20bdca910cfcc580f066034e4ed6
PSesharao <paritalasesharao@gmail.com>
2022-10-07 23:13:09 +0530

    46. Permutations

---
+++ 46. Permutations.cpp
@@ -0,0 +1,115 @@
+/*
+
+https://leetcode.com/problems/permutations/
+
+Approach 1
+
+Run a for loop starting from 0 to nums.size() – 1. Check if the frequency of i is 
+unmarked, if it is unmarked then it means it has not been picked and then we pick. 
+And make sure it is marked as picked.
+
+
+
+Approach 2 
+The recursive idea would be like 
+
+first we consider any index and run a loop to swap with 
+all remaining indices including itself . 
+
+for 1 2 3 
+
+index = 1 
+
+swap(1 , 1 )        swap(1 , 2 )        swap(1 , 3 )
+[1 2 3 ]            [ 2 1 3 ]           [ 3  2 1 ]
+
+At the next level (recursive call ) we need to do it from index + 1
+
+Approach 3
+Coding Ninjas 
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+#define MAX 200010
+
+void init_code()
+{
+    fast_io;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+// Approach 1
+class Solution {
+  private:
+    void recurPermute(vector < int > & ds, vector < int > & nums, vector < vector < int >> & ans, int freq[]) {
+      if (ds.size() == nums.size()) {
+        ans.push_back(ds);
+        return;
+      }
+      for (int i = 0; i < nums.size(); i++) {
+        if (!freq[i]) {
+          ds.push_back(nums[i]);
+          freq[i] = 1;
+          recurPermute(ds, nums, ans, freq);
+          freq[i] = 0;
+          ds.pop_back();
+        }
+      }
+    }
+  public:
+    vector < vector < int >> permute(vector < int > & nums) {
+      vector < vector < int >> ans;
+      vector < int > ds;
+      int freq[nums.size()];
+      for (int i = 0; i < nums.size(); i++) freq[i] = 0;
+      recurPermute(ds, nums, ans, freq);
+      return ans;
+    }
+};
+
+
+// Approach 2
+class Solution {
+  private:
+    void recurPermute(int index, vector < int > & nums, vector < vector < int >> & ans) {
+      if (index == nums.size()) {
+        ans.push_back(nums);
+        return;
+      }
+      for (int i = index; i < nums.size(); i++) {
+        swap(nums[index], nums[i]);
+        recurPermute(index + 1, nums, ans);
+        swap(nums[index], nums[i]);
+      }
+    }
+  public:
+    vector < vector < int >> permute(vector < int > & nums) {
+      vector < vector < int >> ans;
+      recurPermute(0, nums, ans);
+      return ans;
+    }
+};
+
+int main() {
+    init_code() ; 
+    Solution sol ; 
+
+    
+}
