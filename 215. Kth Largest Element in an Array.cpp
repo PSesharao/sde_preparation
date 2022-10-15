a52bb73e53d316f8f9cfec90fe31b27bed73dccd
PSesharao <paritalasesharao@gmail.com>
2022-10-15 20:28:20 +0530

    215. Kth Largest Element in an Array

---
+++ 215. Kth Largest Element in an Array.cpp
@@ -0,0 +1,63 @@
+/*
+
+https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+Time complexity: O(n)  , n = size of array
+
+Time complexity of building heap is O(n) , need to understand this proof 
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+#define MAX 200010
+
+void init_code()
+{
+    fast_io;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int>pq ;
+        int n = arr.size()  ;
+
+        for (int i = 0; i < arr.size(); i++) {
+            pq.push(arr[i])  ;
+        }
+
+        int f = k - 1 ;
+
+        while (f > 0) {
+            pq.pop()  ;
+            f-- ;
+        }
+        return pq.top() ; 
+    }
+};
+
+
+int main() {
+    init_code() ; 
+    Solution sol ; 
+
+    
+}
