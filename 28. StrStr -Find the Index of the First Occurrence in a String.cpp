4e1125fe57d95f027a8168c17ab990ff6daf41c4
PSesharao <paritalasesharao@gmail.com>
2022-10-25 19:54:01 +0530

    28. StrStr -Find the Index of the First Occurrence in a String

---
+++ 28. StrStr -Find the Index of the First Occurrence in a String.cpp
@@ -0,0 +1,65 @@
+/*
+
+https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+
+Time Complexity : O(len1 X len2 ) at the worst case 
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+#define MAX 200010
+
+void init_code()
+{
+    fast_io;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        
+        //bool occured = false ; 
+        int len1 = haystack.size() ; 
+        int len2 = needle.size() ; 
+
+        for(int i=0 ; i<len1 ; i++ ){
+            if( needle[0] == haystack[i] ){
+                int j ; 
+                for(j =0 ; j<len2 ; j++ )
+                    if( haystack[i+j] != needle[j] )
+                        break ; 
+                //occured = true ; 
+                if( j == len2 ) 
+                    return i ; 
+            }
+        }
+
+        return -1 ; 
+
+    }
+};
+
+
+int main() {
+    init_code() ; 
+    Solution sol ; 
+
+    
+}
