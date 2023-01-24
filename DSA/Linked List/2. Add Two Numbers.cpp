f5f39dd8e00373c30d535d7f56c27c093055262d
PSesharao <paritalasesharao@gmail.com>
2023-01-24 22:27:29 +0530

    update on Tue, Jan 24, 2023  10:27:28 PM

--- DSA/Linked List/2. Add Two Numbers.cpp
+++ DSA/Linked List/2. Add Two Numbers.cpp
@@ -33,6 +33,31 @@ void init_code()
 #endif
 }
 
+
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode();
+        ListNode* curr = dummy;
+        int carry = 0;
+        while (l1 || l2) {
+            int x = (l1) ? l1->val : 0;
+            int y = (l2) ? l2->val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+        if (carry > 0) {
+            curr->next = new ListNode(carry);
+        }
+        return dummy->next;
+    }
+};
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
